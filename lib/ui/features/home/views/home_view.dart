e2f1a188c9d3af7afaf4d0afd1bcf16a235763ce
Arnab <arnabmirza2005@gmail.com>
2026-03-31 00:10:04 +0530

    Had to deal with a crap ton of craps: Used Drift database for storing the names of the notebooks, fixed the dependency injection problem with lazy in repository provider, now all the repository providers are provided in the main function and they get executed only at the right time, removed the manually created notebook domain model, fixed animatedgrid to show the notebooks properly, added the ability to delete a notebook, new notebooks are now named as 'New Notebook xx' with xx different if same name already exists and probably not any more

--- lib/ui/features/home/views/home_view.dart
+++ lib/ui/features/home/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:studyapp/data/repositories/notebook_repository.dart';
 import 'package:studyapp/ui/shared/widgets/appbar.dart';
 
+import '../../../../data/database/app_database.dart';
 import '../../../app/cubits/app_cubit.dart';
 import '../cubits/home_cubit.dart';
 
@@ -37,12 +38,21 @@ class _HomeViewState extends State<HomeView> {
           title: "Notebooks (This is the main page)",
           actions: [
             IconButton(
-              onPressed: () {
-                final newid =
-                    context.read<HomeCubit>().state.notebooks.length + 1;
-                context
-                    .read<HomeCubit>()
-                    .addNotebook("New Subject $newid");
+              onPressed: () async {
+                final notebooks =
+                    await context.read<HomeCubit>().getNotebooks();
+                int i = 1;
+                while (true) {
+                  bool nochange = true;
+                  for (Notebook notebook in notebooks) {
+                    if (notebook.name == "New Notebook $i") {
+                      i++;
+                      nochange = false;
+                    }
+                  }
+                  if (nochange) break;
+                }
+                context.read<HomeCubit>().addNotebook("New Notebook $i");
               },
               icon: Icon(Icons.add),
               iconSize: 25,
@@ -57,57 +67,102 @@ class _HomeViewState extends State<HomeView> {
         ),
         body: BlocConsumer<HomeCubit, HomeState>(
           listenWhen: (previous, current) {
-            return previous.notebooks.length < current.notebooks.length;
+            print("prvious: ${previous.notebooks.toString()}");
+            print("current: ${current.notebooks.toString()}");
+            if (previous is HomeReady && current is HomeReady) {
+              return current.notebooks.length == previous.notebooks.length + 1;
+            }
+            return false;
           },
           listener: (BuildContext context, HomeState state) {
             final newIndex = state.notebooks.length - 1;
+            print("state: ${state.notebooks.toString()}");
+            print(newIndex);
             _gridKey.currentState?.insertItem(newIndex);
           },
           builder: (BuildContext context, state) {
-            return Column(
-              children: [
-                Expanded(
-                  child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: AnimatedGrid(
-                        // physics: const NeverScrollableScrollPhysics(),
-                        // itemCount: state.notebooks.length,
-                        key: _gridKey,
-                        initialItemCount: state.notebooks.length,
-                        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                          maxCrossAxisExtent: 150,
-                          mainAxisSpacing: 12,
-                          crossAxisSpacing: 12,
-                        ),
-                        itemBuilder: (BuildContext context, int index,
-                            Animation<double> animation) {
-                          return NotebookItem(
-                              index: index, animation: animation);
-                        },
-                      )),
-                ),
-              ],
-            );
+            if (state is HomeLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (state is HomeReady) {
+              return Column(
+                children: [
+                  Expanded(
+                    child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: AnimatedGrid(
+                          // physics: const NeverScrollableScrollPhysics(),
+                          // itemCount: state.notebooks.length,
+                          key: _gridKey,
+                          initialItemCount: state.notebooks.length,
+                          gridDelegate:
+                              SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 150,
+                            mainAxisSpacing: 12,
+                            crossAxisSpacing: 12,
+                          ),
+                          itemBuilder: (BuildContext context, int index,
+                              Animation<double> animation) {
+                            final Notebook notebook = state.notebooks[index];
+                            return NotebookItem(
+                                index: index,
+                                notebook: notebook,
+                                animation: animation,
+                                gridKey: _gridKey);
+                          },
+                        )),
+                  ),
+                ],
+              );
+            }
+
+            return Center(
+                child: Text(
+                    "The state is neither loading nor ready. What did you do?"));
           },
         ));
   }
 }
 
 class NotebookItem extends StatelessWidget {
+  final Notebook notebook;
   final int index;
   final Animation<double> animation;
+  final GlobalKey<AnimatedGridState> gridKey;
 
-  const NotebookItem({super.key, required this.index, required this.animation});
+  const NotebookItem(
+      {super.key,
+      required this.index,
+      required this.notebook,
+      required this.animation,
+      required this.gridKey});
 
   @override
   Widget build(BuildContext context) {
-    final String text = context.read<HomeCubit>().state.notebooks[index].name;
     return ScaleTransition(
       scale: CurvedAnimation(parent: animation, curve: Curves.easeInOutQuint),
       child: TextButton(
-          onPressed: () {},
-          child: Text(text),
-          style: TextButton.styleFrom(backgroundColor: Colors.amber)),
+        onPressed: () async {
+          await context.read<HomeCubit>().deleteNotebook(notebook.id);
+          gridKey.currentState?.removeItem(
+            index,
+            (context, animation) {
+              return ScaleTransition(
+                scale: animation,
+                child: TextButton(
+                  onPressed: null,
+                  child: Text(notebook.name),
+                  style: TextButton.styleFrom(backgroundColor: Colors.amber),
+                ),
+              );
+            },
+          );
+          
+        },
+        style: TextButton.styleFrom(backgroundColor: Colors.amber),
+        child: Text(notebook.name),
+      ),
     );
   }
 }
